50546c392ed0236c8ad4464444977a6518503d83
jzhang <jzhang@DEV-M-974XF.paychex.com>
2018-01-01 16:51:28 -0500

    add output line

--- CodingPractice/src/firstProject/Animal.java
+++ CodingPractice/src/firstProject/Animal.java
@@ -4,6 +4,6 @@ public class Animal {
 	public static void main(String[]args){
 		System.out.println("how you feel today?");
 		System.out.println("go go go");
-		System.out.println("");
+		System.out.println("study hard.");
 	}
 }
