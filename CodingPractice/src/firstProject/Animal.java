eb3737bc46fcb0ec4453509663bb6409415c4644
jzhang <jzhang@DEV-M-974XF.paychex.com>
2018-01-01 17:10:28 -0500

    goremote

--- CodingPractice/src/firstProject/Animal.java
+++ CodingPractice/src/firstProject/Animal.java
@@ -4,6 +4,7 @@ public class Animal {
 	public static void main(String[]args){
 		System.out.println("how you feel today?");
 		System.out.println("go go go");
+		System.out.println("sfeifnafejeaofn");
 		System.out.println("study hard.");
 	}
 }
